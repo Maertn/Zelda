85e378edc50d2dd772b2cf5f1491e0153c9b2a75
maertn <maartenbouwmans@gmail.com>
2023-03-16 23:00:55 +0100

    added camera and depth to visuals

--- tile.py
+++ tile.py
@@ -6,3 +6,4 @@ class Tile(pg.sprite.Sprite):
         super().__init__(groups)
         self.image = pg.image.load('graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0, -10) #shrinks the hitbox 5 pixels on each side
